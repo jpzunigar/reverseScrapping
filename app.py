7a6ca894d7e7314fbf6da98665de5f04e0d5ae69
jpzunigar <jpzunigar@uc.cl>
2024-03-18 20:34:41 -0500

    adding plots and contracts

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
 from functions import get_info_from_top_position
+from functions import count_values, create_count_plot
 
 
 
@@ -9,8 +10,8 @@ with st.sidebar:
     st.markdown("<h1 style='color: #14F46F; font-size: 22px;'>Scrapping revert.finance data</h1>", unsafe_allow_html=True)
     selected = option_menu(
         menu_title = "Main Menu",
-        options = ["Home","Projects","Contact"],
-        icons = ["house","book","envelope"],
+        options = ["Top Positions"],
+        icons = ["house"],
         menu_icon = "cast",
         default_index = 0,
         styles={
@@ -21,12 +22,26 @@ with st.sidebar:
         }
 
     )
-if selected == "Home":
+    option_red = st.selectbox(
+    'Filtre por red',
+    ('All networks', 'Ethereum', 'Polygon','Arbitrum','Optimism','BNB','EVMOS','Base'))
+if selected == "Top Positions":
     st.header('Revert.Finance Scrapper App')
     st.subheader("Esta hoja resume la información obtenida de top positions de Revert.Finance")
     num_pages = st.number_input('Inserte el numero de paginas que desea scrapear', min_value=1)
     if st.button('Iniciar Scraping', use_container_width=True):
-        df = get_info_from_top_position(num_pages)
-        st.write(df)
+        with st.spinner('Recolectando información...'):
+            df = get_info_from_top_position(num_pages, red = option_red)
+            with st.expander('Ver datos'):
+                st.write(df)
+            col1, col2 = st.columns(2)
+            df_count = count_values(df)
+            with col1:
+                create_count_plot(df_count)
+            with col2:
+                st.dataframe(df_count)
+            
+
+
  
 
--- functions.py
+++ functions.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import pandas as pd
-from streamlit_option_menu import option_menu
 from bs4 import BeautifulSoup as bs
-import requests as rq
-from requests_html import HTMLSession, AsyncHTMLSession
-import asyncio
-
-from selenium import webdriver
 import time
+import altair as alt
 
 
-def get_info_from_top_position(pages):
+def get_info_from_top_position(pages, red = 'All networks'):    
+    if red == 'Ethereum':
+        red = 'ethereum'
+    elif red == 'All networks':
+        red = red
+    else:
+        red = red.lower()
     if pages > 0:
         from selenium import webdriver
         from selenium.webdriver.chrome.options import Options
@@ -24,7 +25,7 @@ def get_info_from_top_position(pages):
         def get_driver():
             return webdriver.Chrome(
             service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+                #ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
             ),
             options=options,
         ) 
@@ -35,14 +36,18 @@ def get_info_from_top_position(pages):
 
         driver = get_driver()
         for i in range(pages):
-            driver.get(f"https://revert.finance/#/top-positions?page={i+1}")
+            if red == 'All networks':
+                driver.get(f"https://revert.finance/#/top-positions/?page={i+1}")
+            else:
+                driver.get(f"https://revert.finance/#/top-positions/{red}/?page={i+1}")
             time.sleep(4.5)  # Let the page render
-
+            #time.sleep(4.5)  # Let the page render
             soup = bs(driver.page_source, 'html.parser')
             lines = soup.find('div', class_=['border', 'hover:bg-gray-90', 'ease-linear'])
             link_elements = lines.findAll('a', class_='text-green-30')
             text_elements = lines.findAll('div', class_='text-ellipsis')
             age = lines.findAll('div', class_='text-sm h-[80px] flex items-center md:h-[60px] md:border-b-transparent justify-end w-28 text-gray-20')
+            contrato_elements = [link_elements[i].get('href').split('/')[-1] for i in range(0,len(link_elements),3)]
             link_elements = [element.text for element in link_elements]
             text_elements = [element.text for element in text_elements]
             age_elements = [element.text.split(' ')[0] for element in age]
@@ -53,6 +58,7 @@ def get_info_from_top_position(pages):
             df2 = pd.DataFrame(nested_text, columns=['PnL', 'APR', 'fee_APR', 'value'])
             df3 = pd.DataFrame(age_elements, columns=['age'])
             df_final_page = pd.concat([df, df2, df3], axis=1)
+            df_final_page['contrato'] = contrato_elements
             # Convert numeric columns to numeric types
             numeric_columns = ['NFT_id', 'PnL', 'APR', 'fee_APR', 'value','age']
 
@@ -60,6 +66,18 @@ def get_info_from_top_position(pages):
                 # Remove commas and dollar signs, then convert to numeric
                 df_final_page[col] = pd.to_numeric(df_final_page[col].replace('[\$,%]', '', regex=True), errors='coerce')
             list_df_all.append(df_final_page)
-        df_final = pd.concat(list_df_all)
-        df_final = df_final.reset_index()
-        return df_final
+
+        return pd.concat(list_df_all).reset_index()
+    
+
+def count_values(df):
+    df1 = df[['pool_fees','contrato']].value_counts().reset_index().head(10)
+    return df1
+
+def create_count_plot(df_count):
+    chart = alt.Chart(df_count).mark_bar().encode(
+        alt.Y('pool_fees', sort=alt.EncodingSortField(field="count", op="count", order='descending')),
+        alt.X('count'),
+        color= alt.value('#14F46F')
+    ).properties(height=450)
+    st.altair_chart(chart, use_container_width=True)
